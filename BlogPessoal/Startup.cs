ca0992c42843d1b71fa042b76ea79c832d524889
Mimiocordeiro <101778002+Mimiocordeiro@users.noreply.github.com>
2022-04-28 15:04:15 -0300

    @MichelliOCordeiro [Task:Criação de Tabelas com ORM EF] (28/04/2022 15h00)

--- BlogPessoal/Startup.cs
+++ BlogPessoal/Startup.cs
@@ -3,11 +3,13 @@ using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using BlogPessoal.src.data;
 
 namespace BlogPessoal
 {
@@ -16,20 +18,25 @@ namespace BlogPessoal
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
+
+        //Configuração bando de dados
         {
             IConfigurationRoot config = new ConfigurationBuilder()
             .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
             .AddJsonFile("appsettings.json")
             .Build();
 
+            services.AddDbContext<BlogPessoalContexto> (opt =>opt.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, BlogPessoalContexto contexto)
         {
             if (env.IsDevelopment())
             {
+                contexto.Database.EnsureCreated();
                 app.UseDeveloperExceptionPage();
             }
 
